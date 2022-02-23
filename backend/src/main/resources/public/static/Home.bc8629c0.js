b179f3ec722abf0385568ead50469fa301f02f10
Michael Kern <michaelkern@t-online.de>
2022-02-23 23:59:07 +0100

    Integrate vue.js in spring-boot
    - use maven as build tool to build and copy frontend resources into the spring static folder
    - missing setup for vite dev server to speed up the dev process

---
+++ backend/src/main/resources/public/static/Home.bc8629c0.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.04b633ed.js";import{o,b as r}from"./vendor.7209080c.js";const c={};function n(t,s){return o(),r("h1",null,"Home")}var m=e(c,[["render",n]]);export{m as default};
